afd4987f95b58d12a8c7cceab38630472582d743
shimhongill <hongil1220@gmail.com>
2024-01-22 19:22:03 +0900

    src 폴더로 분류

--- src/app.js
+++ src/app.js
@@ -7,11 +7,12 @@ const app = express();
 
 
 //라우팅
-const home = require("./routes/home");
+const home = require("../routes/home");
 
 //앱 세팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home);
 
