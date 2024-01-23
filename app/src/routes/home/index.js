af0f99f0bf14c71de605eea0908bf5b4eae85c6b
shimhongill <hongil1220@gmail.com>
2024-01-23 10:45:44 +0900

    로그인을 요청하는 fetch 생성

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -3,13 +3,10 @@
 const express = require("express");
 const router = express.Router();
 
+const ctrl = require("./home.ctrl");
 
-router.get("/", (req, res) => {
-    res.render("home/index");
-});
+router.get("/", ctrl.home);
+router.get("/login", ctrl.login);
 
-router.get("/login", (req, res) => {
-    res.render("home/login");
-});
 
 module.exports = router;
